2b90a592318306129a5b6952bbd2df2aafa57530
Daan Verhoost <daanverhoost@hotmail.com>
2022-11-21 10:26:13 +0100

    Altered script to run as admin on desktop shortcut
    #26

--- DATA/INSTALL/ROGIERS Wizard/SCRIPTS/18___CleanPrintSpoolerShortcut.ps1
+++ DATA/INSTALL/ROGIERS Wizard/SCRIPTS/18___CleanPrintSpoolerShortcut.ps1
@@ -7,3 +7,10 @@ $shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $shortcut.TargetPath = $TargetFile
 $shortcut.IconLocation = "C:\ROGIERS\INSTALL\Windows batch and script\Windows - CleanPrinterSpooler\CleanPrintSpooler.ico"
 $shortcut.Save()
+
+
+# Additional code to enable the flag 'Run As Administrator' on the shortcut properties
+    ### https://stackoverflow.com/questions/28997799/how-to-create-a-run-as-administrator-shortcut-using-powershell
+$bytes = [System.IO.File]::ReadAllBytes("$ShortcutFile")
+$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
+[System.IO.File]::WriteAllBytes("$ShortcutFile", $bytes)
